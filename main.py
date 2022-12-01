d3ee662140a83a00c4e6eb5de8b30fc831ddc954
Shrunga <shrungashree889@gmail.com>
2022-12-01 17:21:07 +0000

    A complete Rock Paper scissors Game.Using Some basic of Python

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
-def get_choices():
-  player_choice = input('Enter a choice (rock,paper,scissors): ')
-  computer_choice = 'paper' 
-  choice = {"player":player_choice,"computer":computer_choice}
+import random #to pick the random in btw rock paper scissor
 
-  return choice
+
+def get_choices(): #defing the function
+  player_choice = input('Enter a choice (rock,paper,scissors): ')
+  options = ["rock", "paper", "scissor"]
+  computer_choice = random.choice(options)
+  choices = {"player": player_choice, "computer": computer_choice}
+  return choices
+
+
+def check_win(player,computer):
+  print(f"you chose {player}, computer choose {computer}")
+  if player == computer:
+    return "its a tie!"
+    
+  #elif player == "rock" and computer == "scissor":
+   # return "Rock takes down scissors! You Win"
+ # elif player == "rock" and computer == "paper":
+    #return "Paper takes down Rock! You lose"
   
-choice = get_choices()
-print(choice) 
+  elif player == "rock":
+    if computer == "scissor":
+      return "Rock takes down scissors! You Win"
+    else:
+      return "Paper takes down Rock! You lose"
+  
+  elif player == "paper":
+     if computer == "rock":
+      return "paper takes down rock! You Win"
+     else:
+       return "scissors takes down paper! You lose"
+
+    
+  elif player == "scissors":
+     if computer == "paper":
+       return "scissors takes down paper! You Win"
+     else:
+       return "Rock takes down scissors! You lose"
+  
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"])
+
+print(result)
